9fdb1d72c99516d5a3f05646701ec57d7c2a7d78
Juan Gonon <juanfegonon27@gmail.com>
2025-09-10 20:18:28 -0600

    react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { AppRouter } from './router/AppRouter'
+
 function App() {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <AppRouter />
     </>
   )
 }
---
+++ src/router/AppRouter.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router'
+import { LoginPage } from '../features/auth/LoginPage'
+import { DashboardPage } from '../features/dashboard/DashboardPage'
+export const AppRouter = () => {
+  const [status] = useState(true)
+  return (
+    <BrowserRouter>
+      <Routes>
+        {status ? (
+          <>
+            <Route path='/auth/*' element={<LoginPage />}></Route>
+            <Route path='/*' element={<Navigate to='/auth/login' />} />
+          </>
+        ) : (
+          <>
+            <Route path='/' element={<DashboardPage />}></Route>
+            <Route path='/*' element={<Navigate to='/' />} />
+          </>
+        )}
+      </Routes>
+    </BrowserRouter>
+  )
+}
