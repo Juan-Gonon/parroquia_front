3e7595ff8ba03877b2bb0c35d83ad201284f598c
Juan Gonon <juanfegonon27@gmail.com>
2025-09-12 11:16:03 -0600

    auth slice

--- src/app/auth/authSlice.js
+++ src/app/auth/authSlice.js
@@ -3,19 +3,31 @@ import { createSlice } from '@reduxjs/toolkit'
 export const authSlice = createSlice({
   name: 'authSlice',
   initialState: {
-    counter: 10,
+    status: 'checking', // 'authenticated', 'not-authenticated'
+    user: {},
+    errorMessage: undefined,
   },
   reducers: {
-    increment: (state /* action */) => {
-      //! https://react-redux.js.org/tutorials/quick-start
-      // Redux Toolkit allows us to write 'mutating' logic in reducers. It
-      // no muta realmente el estado porque usa la biblioteca Immer,
-      // que detecta cambios en un 'draft state' y produce un nuevo
-      // estado inmutable basado en esos cambios
-      state.counter += 1
+    onChecking: (state) => {
+      ;((state.status = 'checking'),
+        (state.user = {}),
+        (state.errorMessage = undefined))
+    },
+    onLogin: (state, { payload }) => {
+      ;((state.status = 'authenticated'),
+        (state.user = payload),
+        (state.errorMessage = undefined))
+    },
+    onLogout: (state, payload) => {
+      ;((state.status = 'not-authenticated'),
+        (state.user = {}),
+        (state.errorMessage = payload))
+    },
+    clearErrorMessage: (state) => {
+      state.errorMessage = undefined
     },
   },
 })
 
-// Los creadores de acciones se generan para cada función reductora de casos
-export const { increment } = authSlice.actions
+export const { onChecking, onLogin, onLogout, clearErrorMessage } =
+  authSlice.actions
