6f91a8a7a9ed680493e9e082a33a79adddd0bfe3
Juan Gonon <juanfegonon27@gmail.com>
2025-09-17 14:49:38 -0600

    modal storage ui

--- src/app/storaje.js
+++ src/app/storaje.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { authSlice } from './auth'
 import { themeSlice } from './Theme/themeSlice'
+import { modalSlice } from './modalUI/modalSlice'
 
 export const store = configureStore({
   reducer: {
     auth: authSlice.reducer,
     theme: themeSlice.reducer,
+    uiModal: modalSlice.reducer,
   },
 })
