3e902381b6d43ffe28ee4d90f209aba7b47dd31b
Juan Gonon <juanfegonon27@gmail.com>
2025-09-17 14:55:04 -0600

    component search

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,18 +1,13 @@
 import styled from 'styled-components'
 import { useThemeStore } from '../hook/useThemeStore'
-import { MdAdd } from 'react-icons/md' // Importamos un ícono para el botón de agregar
-import { IoIosSearch } from 'react-icons/io' // Importamos un ícono para el buscador
+import { MdAdd } from 'react-icons/md'
+import { Search } from '../components/Search'
 
 export const Navbar = () => {
   const { theme } = useThemeStore()
   return (
     <Container $themeUse={theme}>
-      <SearchSection>
-        <SearchInputContainer>
-          <IoIosSearch />
-          <input type='text' placeholder='Buscar personal...' />
-        </SearchInputContainer>
-      </SearchSection>
+      <Search />
       <ActionsSection>
         <AddButton>
           <MdAdd />
@@ -37,44 +32,6 @@ const Container = styled.main`
   box-shadow: 0 2px 5px rgba(0, 0, 0, 0.05);
   z-index: 100;
   /* background: red; */
-`
-
-const SearchSection = styled.section`
-  flex-grow: 1;
-  max-width: 400px;
-`
-
-const SearchInputContainer = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 10px;
-  background-color: ${({ theme }) => theme.bg3};
-  padding: 8px 15px;
-  border-radius: 8px;
-  border: 1px solid ${({ theme }) => theme.bg3};
-  transition: border-color 0.3s ease;
-
-  &:focus-within {
-    border-color: ${({ theme }) => theme.primary};
-  }
-
-  svg {
-    color: ${({ theme }) => theme.gray500};
-    font-size: ${({ theme }) => theme.fontmd};
-  }
-
-  input {
-    flex-grow: 1;
-    background: transparent;
-    border: none;
-    outline: none;
-    color: ${({ theme }) => theme.text};
-    font-size: ${({ theme }) => theme.fontsm};
-
-    &::placeholder {
-      color: ${({ theme }) => theme.gray500};
-    }
-  }
 `
 
 const ActionsSection = styled.section`
