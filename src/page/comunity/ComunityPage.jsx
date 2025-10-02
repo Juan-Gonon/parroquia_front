7ec4efc252dc6ff7690f09eca042e6b677dc642c
Juan Gonon <juanfegonon27@gmail.com>
2025-10-02 08:55:56 -0600

    modif navbar

--- src/page/comunity/ComunityPage.jsx
+++ src/page/comunity/ComunityPage.jsx
@@ -5,7 +5,13 @@ import { ComunitySection } from '../../features/comunity/ComunitySection'
 export const ComunityPage = () => {
   return (
     <Container>
-      <Navbar></Navbar>
+      <Navbar>
+        <h1>Comunidad de Fe</h1>
+        <p>
+          Donde dos o tres se reúnen en mi nombre, alli estoy yo en medio de
+          ellos <br /> Mateo 18:20
+        </p>
+      </Navbar>
       <ComunitySection></ComunitySection>
     </Container>
   )
