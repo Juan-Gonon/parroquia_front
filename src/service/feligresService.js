92bde562447ba76cb4520e96c3f8231f3990ded7
Juan Gonon <juanfegonon27@gmail.com>
2025-10-17 19:30:57 -0600

    services feligres

---
+++ src/service/feligresService.js
@@ -0,0 +1,40 @@
+import parishApi from '../api/api'
+
+export const getAllFeligresService = async ({ page = 1, limit = 10 }) => {
+  try {
+    const res = await parishApi.get('/feligres', { params: { page, limit } })
+    if (res.status !== 200) throw new Error('Error al obtener feligreses')
+    return res.data
+  } catch (error) {
+    const message =
+      error.response?.data?.error || error.message || 'Error desconocido'
+    throw new Error(message)
+  }
+}
+
+export const createFeligresService = async ({ data }) => {
+  try {
+    const res = await parishApi.post('/feligres', data)
+    return res.data
+  } catch (err) {
+    return err.response?.data || { message: 'Error al crear feligrés' }
+  }
+}
+
+export const updateFeligresService = async ({ id, data }) => {
+  try {
+    const res = await parishApi.put(`/feligres/${id}`, data)
+    return res.data
+  } catch (err) {
+    return err.response?.data || { message: 'Error al actualizar feligrés' }
+  }
+}
+
+export const deleteFeligresService = async ({ id }) => {
+  try {
+    const res = await parishApi.delete(`/feligres/${id}`)
+    return res.data
+  } catch (err) {
+    return err.response?.data || { message: 'Error al eliminar feligrés' }
+  }
+}
